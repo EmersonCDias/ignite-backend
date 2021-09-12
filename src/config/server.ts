b1866075d54274018142fb3ad8973105aa8ba061
EmersonCDias <emerson_cdias@hotmail.com>
2021-09-12 00:49:58 -0300

    add Procfile

---
+++ src/config/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+import categoriesRoutes from '../routes/categories.routes';
+
+const PORT: string | number = process.env.PORT || 5000;
+const app = express();
+
+app.use(express.json());
+
+app.post('/categories', categoriesRoutes);
+
+app.listen(PORT, () => console.log(`Hosting @${PORT}`));
