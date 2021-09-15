4c6a93c4c9c6ac8a1d08a5db8533107cbaf7f271
EmersonCDias <emerson_cdias@hotmail.com>
2021-09-14 22:12:14 -0300

    refact routes

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,14 +1,11 @@
 import express from 'express';
 
-import categoriesRoutes from '../modules/cars/routes/categories.routes';
-import specificationsRoutes from '../modules/cars/routes/services.routes';
+import routes from '../routes';
 
 const PORT: string | number = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
-
-app.use('/categories', categoriesRoutes);
-app.use('/specifications', specificationsRoutes);
+app.use(routes);
 
 app.listen(PORT, () => console.log(`Hosting @${PORT}`));
